7e92cd0116068b1e8efddc6186ca7bdbc56bd231
symnbe <ookaminingen@me.com>
2023-02-20 15:12:18 +0000

    [Add]アカウント画像追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
     @books = Book.all
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -21,11 +23,11 @@ class BooksController < ApplicationController
 
   def edit
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,27 @@
 class UsersController < ApplicationController
   def index
+    @user - User.all
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
   end
 
   def edit
+  end
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+
+
+  private
+  def users_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
   end
 end
